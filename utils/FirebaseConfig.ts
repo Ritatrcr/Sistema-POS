8d9661e32d2730ec557ac6ff38220b2225f782f0
Brandon Eduardo Merchan sandoval <merchito@Brandons-MacBook-Pro.local>
2025-03-19 23:05:05 -0500

    Reset del proyecto, organizacion carpetas y conexion Firebase

---
+++ utils/FirebaseConfig.ts
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfTXlmYHaN_4moTJNyRGeeEjd6XlhRZhc",
+  authDomain: "sistema-pos-69026.firebaseapp.com",
+  projectId: "sistema-pos-69026",
+  storageBucket: "sistema-pos-69026.firebasestorage.app",
+  messagingSenderId: "706222144600",
+  appId: "1:706222144600:web:2651c56b3af0c29b5e9e2d",
+  measurementId: "G-8LQMSF0774"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
